321fcd22e6e7386c8b8415ebc3734420bd695d01
JIA-office <callmexiao@outlook.com>
2023-03-24 17:02:04 +0800

    this指针

--- study/study/008_9-类和对象-对象特性-this指针.cpp
+++ study/study/008_9-类和对象-对象特性-this指针.cpp
@@ -20,8 +20,42 @@
   *		当形参和成员变量同名时,使用this指针来区分
   *		类的非静态成员函数中返回对象本身,可使用return *this
   */
+class Person
+{
+public:
+	Person(int age)
+	{
+		//age = age; // 这种情况下 编译器根本无法区分这里的age 是不是成员变量 age
+		this->age = age; // 这里的this的作用就是 解决名称冲突 告诉编译器 我指向的是Person的成员属性 age
+	}
+
+	Person* PersonAddAge(Person& person)
+	{
+		this->age += person.age; //将调用的对象的年龄与传入的对象的年龄进行相加 this指向的是调用的对象
+		return this; //此时的this指向的是调用的对象 返回他的指针
+	}
+
+	int age;
+};
+
+void test1()
+{
+	Person p(18);
+	std::cout << "p的年龄是:" << p.age << std::endl;
+}
+
+void test2()
+{
+	Person p1(10);
+	Person p2(12);
+	p1.PersonAddAge(p1);
+	std::cout << "p1的年龄为:" << p1.age << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
+	test1();
+	test2();
 	system("pause");
 	return 0;
 }
