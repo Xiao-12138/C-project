c816a2ea53866f0a14300844813db343e5da77cf
JIA-office <callmexiao@outlook.com>
2023-05-20 10:48:04 +0800

    JIA
    forgot

---
+++ study/study/008_14-类和对象-友元-成员函数做友元.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+/*
+ * 文件名称:_008_14_类和对象_友元_成员函数做友元
+ * 创建时间:2023/2/28 15:11:25
+ * by:JIA
+ *
+ *
+ *
+ * 在程序里有些私有属性也想让类外特殊的一些函数或者类进行访问，就需要用到友元的技术
+ *
+ * 友元的目的就是让一个函数或者类 访问另一个类中的私有成员
+ *
+ * 友元的关键字为：friend
+ *
+ * 友元的三种实现
+ *		全局函数做友元
+ *		类做友元
+ *		成员函数做友元
+ *
+ */
+
+ //成员函数做友元
+
+ //声明MyHouse类以便于在好朋友类中使用
+class MyHouse;
+
+//好朋友类
+class MygoodFriends
+{
+public:
+	void visit();
+	MygoodFriends();
+	MyHouse* my_house;
+};
+
+//MyHouse
+class MyHouse
+{
+	//构造函数
+
+	//告诉编译器MyGoodFriends类的visit函数是我的友元,允许访问我的私有成员
+	friend void MygoodFriends::visit();
+
+public:
+	MyHouse();
+	std::string m_SittingRoom;
+private:
+	std::string m_BedRoom;
+};
+
+//类外实现成员函数
+//实现MyHouse的构造函数
+MyHouse::MyHouse()
+{
+	m_SittingRoom = "客厅";
+	m_BedRoom = "卧室";
+}
+//实现好朋友类的构造函数
+MygoodFriends::MygoodFriends()
+{
+	//在好朋友类的构造函数类中实例化MyHouse
+	my_house = new MyHouse;
+}
+//实现visit成员函数
+void MygoodFriends::visit()
+{
+	std::cout << "好朋友访问了:" << my_house->m_SittingRoom << std::endl;
+	std::cout << "好朋友访问了:" << my_house->m_BedRoom << std::endl;
+}
+
+void test()
+{
+	MygoodFriends mygood_friends;
+	mygood_friends.visit();
+}
+int main(int argc, char* argv[])
+{
+	test();
+	return 0;
+	system("pause");
+}
---
+++ study/study/008_15-类和对象-C++运算符重载-加号运算符重载.cpp
@@ -0,0 +1,77 @@
+﻿/*
+ * 文件名称:_008_15_类和对象_C__运算符重载_加号运算符重载
+ * 创建时间:2023/2/28 15:11:25
+ * by:JIA
+ *
+ */
+#include <iostream>
+
+ /*
+  * 运算符重载概念:对已有的运算符重新进行定义,赋予其另一种功能,以适应不同的数据类型
+  *
+  *加号运算符重载
+  *
+  *运算符的重载也能进行函数的重载
+  *
+  *作用:实现两个自定义数据类型相加的运算
+  *
+  */
+
+class Person
+{
+public:
+	int m_a;
+	int m_b;
+
+	//成员函数对+号运算符进行重载
+	//Person operator+ (Person&person)
+	//{
+	//	Person temp;
+	//	temp.m_a = this->m_a + person.m_a;
+	//	temp.m_b = this->m_b + person.m_b;
+	//	return person;
+	//}
+};
+
+// 全局函数对+号进行重载
+Person operator+(Person& p1, Person& p2)
+{
+	Person temp;
+	temp.m_a = p1.m_a + p2.m_a;
+	temp.m_b = p1.m_b + p2.m_b;
+	return temp;
+}
+
+//进行函数重载
+Person operator+(Person& p1, int num)
+{
+	Person temp;
+	temp.m_b += num;
+	temp.m_a += num;
+	std::cout << typeid(temp).name() << std::endl;
+	return temp;
+}
+
+void test()
+{
+	Person p1{ 1, 1 };
+	Person p2{ 2, 2 };
+	Person p3;
+	p3 = p1 + p2; //当使用成员函数重载时:实际调用为p3 = p1.operator+(p2);当使用全局函数重载时:实际调用为:p3 = operator+(p1,p2);
+	std::cout << p3.m_a << p3.m_b << std::endl;
+}
+
+void test01()
+{
+	Person p1{ 10, 10 };
+	Person p2 = p1 + 20;
+	std::cout << p2.m_b << p2.m_b << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+	test();
+	test01();
+	return 0;
+	system("pause");
+}
